dd618d40eadca4563b97bb69d1cc80b1332dd32b
dan-and224 <anddaniil10@gmail.com>
2024-11-29 16:06:59 +0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+function symbols() {
+    window.open("symbols.html");
+}
+
+function game() {
+    window.open("game-iframe.html");
+}
+
+function telegram() {
+    window.open("https://t.me/daniilzenteleram");
+}
+
+function challels() {
+    window.open("chl.html");
+}
+
+function dzen() {
+    window.open("https://dzen.ru/daniilandry");
+}
+
+function gamejolt() {
+    window.open("https://gamejolt.com/@DaniilAndryushchenko");
+}
+
+function yt() {
+    window.open("https://www.youtube.com/@%D0%90%D1%80%D1%87%D0%B8%D0%9A%D0%BE%D1%82-24");
+}
+
+function eeeee() {
+    window.open("/downloads/EEEEE.exe");
+}
